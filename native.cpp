cfe7ed714648fd8291a5277d618e27b6db73f7f7
Nader Mahbub Khan <muhammadnadermahbubkhan@gmail.com>
2024-08-11 01:12:59 -0400

    fix

--- native.cpp
+++ native.cpp
@@ -5,6 +5,12 @@ extern "C" {
         printf("%lld\n", val);
     }
 
+    //square
+    void square(long long val) {
+        long long cal = val * val;
+        printf("%lld\n", cal);
+    }
+
     // can print string
     void printStr(const char* val) {
         printf("%s\n", val);
